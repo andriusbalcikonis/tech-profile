7a0ad6abf3d4f2575be390a1d9e4790ecda9fb03
Andrius Balcikonis <andrius@balcikonis.com>
2020-07-27 23:45:21 +0300

    Basketball model

--- poc/django-orm/queries/queries_app/models.py
+++ poc/django-orm/queries/queries_app/models.py
@@ -1,10 +1,95 @@
 
 from django.db import models
+from enum import Enum
 
 
-class Person(models.Model):
+class Positions(Enum):
+    CN = "Center"
+    PF = "Power forward"
+    SF = "Small forward"
+    SG = "Shooting guard"
+    PG = "Point guard"
+
+
+class Player(models.Model):
+
     first_name = models.TextField()
+
     last_name = models.TextField()
+
+    height = models.IntegerField()
+
+    weight = models.IntegerField()
+
+    date_of_birth = models.DateField()
+
+    position = models.CharField(
+        max_length=5,
+        choices=[(pos, pos.value) for pos in Positions]
+    )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Team(models.Model):
+
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Contract(models.Model):
+
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="contracts")
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="contracts")
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.player} in {self.team}'
+
+
+class Arena(models.Model):
+
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+
+    date = models.DateField()
+
+    arena = models.ForeignKey(Arena, on_delete=models.CASCADE, related_name="games")
+
+    attendance = models.IntegerField()
+
+    final_score_home = models.IntegerField()
+
+    final_score_away = models.IntegerField()
+
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="home_games")
+
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="away_games")
+
+    def __str__(self):
+        return f'{self.home_team} vs {self.away_team} on {self.date}'
+
+
+class StatLine(models.Model):
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="stat_lines")
+
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="game_stats")
+
+    points = models.IntegerField()
+
+    rebounds = models.IntegerField()
+
+    assists = models.IntegerField()
--- poc/django-orm/queries/queries_app/tests.py
+++ poc/django-orm/queries/queries_app/tests.py
@@ -1,17 +1,33 @@
+from datetime import date
 from django.test import TestCase
-from queries.queries_app.models import Person
+from queries.queries_app.models import Player, Team, Positions
 
 
 class QueriesTestCase(TestCase):
+
+    # Setup:
+
+    def setUp(self):
+        self.team1 = Team(name="Sakalai")
+        self.team1.save()
+        self.team1_player1 = Player(first_name="Rolandas", last_name="Matulis", height=200,
+                                    weight=120, date_of_birth=date(1970, 1, 1),
+                                    position=Positions.CN)
+        self.team1_player1.save()
+        self.team1_player2 = Player(first_name="Rolandas", last_name="Skaisgirys",
+                                    height=190, weight=90, date_of_birth=date(1970, 1, 1),
+                                    position=Positions.SG)
+        self.team1_player2.save()
+        self.team1_player3 = Player(first_name="Justas", last_name="Sinica",
+                                    height=200, weight=120, date_of_birth=date(1970, 1, 1),
+                                    position=Positions.SF)
+        self.team1_player3.save()
 
     # Test methods:
 
     def test_trivial_query(self):
 
-        p = Person(first_name="A", last_name="B")
-        p.save()
-
-        query = Person.objects.all()
+        query = Player.objects.all()
         results = list(query)
 
-        self.assertEqual(len(results), 1)
+        self.assertEqual(len(results), 3)
